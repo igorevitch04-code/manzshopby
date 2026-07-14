85957cea3967281abfa5f21c6617849bc11c6efe
igorevitch04-code <igorevitch04@gmail.com>
2026-07-14 20:12:05 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const getTelegramUser = () => {
 const money = (v) => v.toLocaleString("ru-RU") + " Br";
 const getBrands = (products) => [...new Set(products.map(p => p.brand))];
 
-const ADMIN_IDS = [123456789, 987654321]; // замените на свои
+const ADMIN_IDS = [778715828, 987654321]; // замените на свои
 
 const DEFAULT_PRODUCTS = [
   { id: 1, brand: "NIKE", name: "Dunk Low Panda", price: 18990, oldPrice: null, image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff", sales: 120, ratings: [], averageRating: 0, description: "Классические Nike Dunk Low Panda.", sizes: ["40","41","42","43","44"] },
