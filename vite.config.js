117974f0423d176f4c98a2ea043dc94c69237a22
igorevitch04-code <igorevitch04@gmail.com>
2026-07-15 18:04:23 +0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       'react-native': 'react-native-web',
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
     },
   },
 });
